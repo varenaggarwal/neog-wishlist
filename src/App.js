de1a93d7e42cd4e810a81044ef9f7ba545135c4c
Varen Aggarwal <varenaggarwal@gmail.com>
2021-03-23 18:58:55 +0530

    remove destructing + move to js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./styles.css";
 
 export default function App() {
   const { state, dispatch } = useCartData();
-  const { response } = useProductLoader();
+  const response = useProductLoader();
 
   return (
     <div className="App">
